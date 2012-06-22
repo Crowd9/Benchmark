5b7afe6b99be84a91103b709153660d2aa839e3e
John Sherwood <john.sherwood.was.taken@gmail.com>
2012-06-22 10:21:33 +1000

    Yumming some dependencies

--- script.sh
+++ script.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
-PID=`cat .pid`
+echo "Checking for required dependencies"
+
+TO_INSTALL=""
+
+if [ `perl -MTime::HiRes -e 1 2>/dev/null; echo $?` -ne 0 ]; then
+  TO_INSTALL="$TO_INSTALL perl-Time-HiRes"
+fi
+
+if [ `which make 2>/dev/null; echo $?` -ne 0 ]; then
+  TO_INSTALL="$TO_INSTALL make"
+fi
+
+if [ $TO_INSTALL != '' ]; then
+  echo "Using yum to install$TO_INSTALL"
+  yum install -y $TO_INSTALL
+fi 
+
+
+PID=`cat .pid 2>/dev/null`
 UNIX_BENCH_VERSION='5.1.3'
 UNIX_BENCH_DIR=UnixBench-$UNIX_BENCH_VERSION
 UPLOAD_ENDPOINT='http://promozor.com/uploads.text'
@@ -26,17 +44,17 @@ echo "This can take several hours.  ServerBear will email you when it's done."
 echo "You can log out/Ctrl-C any time while this is happening (it's running through nohup)."
 
 cd $UNIX_BENCH_DIR
-echo "distro: " > sz-output.log
-cat /etc/issue >> sz-output.log
-echo "disk space: " >> sz-output.log
-df --total >> sz-output.log
-echo "free: " >> sz-output.log
-free >> sz-output.log
+echo "distro: " > sb-output.log
+cat /etc/issue >> sb-output.log
+echo "disk space: " >> sb-output.log
+df --total >> sb-output.log
+echo "free: " >> sb-output.log
+free >> sb-output.log
 
-./Run >> sz-output.log 2> sz-error.log
+./Run >> sb-output.log 2> sb-error.log
 
-RESPONSE=\`curl -s -F "upload[upload_type]=unix-bench-output" -F "upload[data]=<sz-output.log" -F "upload[key]=$SZK" $UPLOAD_ENDPOINT\`
-RESPONSE=\`curl -s -F "upload[upload_type]=unix-bench-error" -F "upload[data]=<sz-error.log" -F "upload[key]=$SZK" $UPLOAD_ENDPOINT\`
+RESPONSE=\`curl -s -F "upload[upload_type]=unix-bench-output" -F "upload[data]=<sb-output.log" -F "upload[key]=$SBK" $UPLOAD_ENDPOINT\`
+RESPONSE=\`curl -s -F "upload[upload_type]=unix-bench-error" -F "upload[data]=<sb-error.log" -F "upload[key]=$SBK" $UPLOAD_ENDPOINT\`
 
 echo "Uploading results..."
 echo "Response: \$RESPONSE"
@@ -47,8 +65,8 @@ EOF
 
 chmod u+x run-upload.sh
 
-nohup ./run-upload.sh > sz-script.log &
+nohup ./run-upload.sh > sb-script.log &
 
 echo $! > .pid
 
-tail -f sz-script.log
+tail -f sb-script.log
